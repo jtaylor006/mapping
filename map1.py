8c2aff3d1b12fda7d618f2df4412c1f1d4c51bfd
jtaylor6 <jarrede20@gmail.com>
2021-01-07 17:32:25 -0500

    added multiple points internally

--- map1.py
+++ map1.py
@@ -2,7 +2,10 @@ import folium
 map = folium.Map(location=[38.63, -90.20], zoom_start=6, tiles = "Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[33.75, -84.38], popup="I created this marker", icon=folium.Icon(color='green')))
+
+for coordinates in [[33.75, -84.34], [35.75, -80.38]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="I created this marker", icon=folium.Icon(color='green')))
+
 map.add_child(fg)
 
 map.save("Map1.html")
