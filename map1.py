5bd3aca71a597a83559c998b2d46408507a617d1
jtaylor6 <jarrede20@gmail.com>
2021-01-08 20:45:38 -0500

    made the map choropleth capable

--- map1.py
+++ map1.py
@@ -3,24 +3,31 @@ import pandas
 
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
-lon= list(data["LON"])
+lon = list(data["LON"])
 elev = list(data["ELEV"])
+
 
 def color_producer(elevation):
     if elevation < 1000:
         return 'green'
-    elif 1000<= elevation < 3000:
+    elif 1000 <= elevation < 3000:
         return 'orange'
     else:
         return 'red'
-map = folium.Map(location=[38.63, -90.20], zoom_start=5.5, tiles = "Stamen Terrain")
+
+
+map = folium.Map(location=[38.63, -90.20],
+                 zoom_start=5.5, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln, el in zip(lat,lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=7, popup=folium.Popup(str(el)+" m",parse_html=True), fill_color=color_producer(el), color = 'grey', fill_opacity=0.7))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=7, popup=folium.Popup(str(
+        el)+" m", parse_html=True), fill_color=color_producer(el), color='grey', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='UTF-8').read())))
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='UTF-8').read(),
+                            style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
+                                                      else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map.add_child(fg)
 map.save("Map1.html")
