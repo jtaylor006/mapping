f3278508cb07238eb0ced5532185db8eb1bdf556
jtaylor6 <jarrede20@gmail.com>
2021-01-08 19:05:02 -0500

    made the color of the pointer correlate w/ elevation

--- map1.py
+++ map1.py
@@ -6,12 +6,21 @@ lat = list(data["LAT"])
 lon= list(data["LON"])
 elev = list(data["ELEV"])
 
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000<= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+    
+
 map = folium.Map(location=[38.63, -90.20], zoom_start=5.5, tiles = "Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat,lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(str(el)+" m",parse_html=True), icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(str(el)+" m",parse_html=True), icon=folium.Icon(color=color_producer(el))))
 
 map.add_child(fg)
 
